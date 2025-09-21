935a71b4446472bc00443ded22ca31ce606611a6
ThibeauWouters <thibeauwouters@gmail.com>
2025-09-21 22:13:26 +0200

    Varying spin runs done

--- jester/outdir_GW170817_GW190425_GW231109/submit.sh
+++ jester/outdir_GW170817_GW190425_GW231109/submit.sh
@@ -27,7 +27,7 @@ python inference.py \
     --sample-GW170817 True \
     --sample-GW190425 True \
     --sample-GW231109 True \
-    --GW231109-NF-filepath ./NFs/prod_BW_XP_s005_l5000_default/ \
+    --GW231109-NF-filepath ./NFs/prod_BW_XP_s005_l5000_default/model.eqx \
     --sample-radio True \
 
 python postprocessing.py outdir_GW170817_GW190425_GW231109
