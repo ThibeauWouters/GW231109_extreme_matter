9732bd5537629a3faa157dc3418f8d5ab0756567
ThibeauWouters <thibeauwouters@gmail.com>
2026-06-11 11:32:03 +0200

    Adding submit files

---
+++ referee/3G_reruns/et_ce/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH -J et_ce_xasinj_gw231109
+#SBATCH -o logs/et_ce_xasinj_gw231109.log
+#SBATCH -e logs/et_ce_xasinj_gw231109.err
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --cpus-per-task 192
+#SBATCH -p cpu
+
+source /work/wouters/projects/19_GW231109_referee/.venv/bin/activate
+
+export MKL_NUM_THREADS=1
+export NUMEXPR_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+
+srun python bns_mb.py
